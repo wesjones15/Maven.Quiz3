395027e413081e81d5872140a6fea328ce47cc0f
wes <wes.jones1597@gmail.com>
2019-11-18 14:14:36 -0500

    TicTacToe tests passing

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -1,37 +1,82 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    private String[][] board;
 
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        return board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] column = new String[board[0].length];
+        for (int i = 0; i < board[0].length; i++) {
+            column[i] = board[i][value];
+        }
+        return column;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+        String[] row = getRow(rowIndex);
+        return isHomogenous(row);
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        String[] column = getColumn(columnIndex);
+        return isHomogenous(column);
     }
 
     public String getWinner() {
+        if (isDiagonalHomogenous()) return getDiagonal()[0];
+        if (isAntiDiagonalHomogenous()) return getAntiDiagonal()[0];
+        for (int i = 0; i < board[0].length; i++) {
+            if (isColumnHomogeneous(i)) return getColumn(i)[0];
+            if (isRowHomogenous(i)) return getRow(i)[0];
+        }
         return null;
     }
 
     public String[][] getBoard() {
-        return null;
+        return this.board;
+    }
+
+    public Boolean isDiagonalHomogenous() {
+        return isHomogenous(getDiagonal());
+    }
+
+    public Boolean isAntiDiagonalHomogenous() {
+        return isHomogenous(getAntiDiagonal());
+    }
+
+    public Boolean isHomogenous(String[] array) {
+        return Arrays.stream(array).filter(item -> item.equals(array[0])).count() == array.length;
+    }
+
+    public String[] getDiagonal() {
+        String[] diagonal = new String[board[0].length];
+        for (int i = 0; i < board[0].length; i++) {
+            diagonal[i] = board[i][i];
+        }
+        return diagonal;
+    }
+
+    public String[] getAntiDiagonal() {
+        String[] diagonal = new String[board[0].length];
+        for (int i = 0; i < board[0].length; i++) {
+            Integer index = board[0].length-1-i;
+            diagonal[i] = board[index][i];
+        }
+        return diagonal;
     }
 }
