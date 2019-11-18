708c80c94c72884221f8b0ba479c3d97ad944e90
wes <wes.jones1597@gmail.com>
2019-11-18 16:23:30 -0500

    string utils

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -8,18 +8,40 @@ import java.util.List;
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+        return str.substring(0,indexToCapitalize) + String.valueOf(str.charAt(indexToCapitalize)).toUpperCase() + str.substring(indexToCapitalize+1);
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+        return (baseString.charAt(indexOfString) == characterToCheckFor);
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+        String out = "";
+        for (int i = 0; i < string.length()-1; i++) {
+            for (int j = i+1; j <= string.length(); j++) {
+                String sub = string.substring(i,j);
+                if (!out.contains(" "+sub+" ")) out = out + " " + sub;
+            }
+        }
+        out = out + " " + string.substring(string.length()-1);
+        return out.trim().split(" ");
     }
 
+//    public static Integer getNumberOfOccurrances(ArrayList<String> array, String str) {
+//        Integer count = 0;
+//        for (String item : array) {
+//            if (item.equals(str)) count++;
+//        }
+//        return count;
+//    }
+
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+        Integer num = input.length();
+        Integer out = 0;
+        while (num >0) {
+            out += num;
+            num --;
+        }
+        return out-1;//getAllSubStrings(input).length;
     }
 }
