bea0c07b8690b7deb55ee70438366a96109a16ba
wes <wes.jones1597@gmail.com>
2019-11-18 16:44:10 -0500

    blah

--- src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
@@ -1,6 +1,9 @@
 package rocks.zipcode.io.quiz3.generics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author leon on 09/12/2018.
@@ -13,17 +16,30 @@ public class ArrayUtility<SomeType> {
     }
 
     public SomeType findOddOccurringValue() {
+        Integer count = 0;
+        for (SomeType value : array) {
+            count = getNumberOfOccurrences(value);
+            if (count % 2 != 0) return value;
+        }
         return null;
     }
 
     public SomeType findEvenOccurringValue() {
+        Integer count = 0;
+        for (SomeType value : array) {
+            count = getNumberOfOccurrences(value);
+            if (count % 2 == 0) return value;
+        }
         return null;
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
-        return null;
+        return (int) Arrays.stream(array)
+                .filter(val -> val == valueToEvaluate)
+                .count();
     }
 
+    //TODO come vack to this one
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
         return null;
     }
