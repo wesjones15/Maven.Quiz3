1f0419abf2006ff493bc516642ea971bf75d7b4b
wes <wes.jones1597@gmail.com>
2019-11-18 17:09:17 -0500

    RockPaperScissorEValuator

--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
@@ -4,13 +4,46 @@ package rocks.zipcode.io.quiz3.objectorientation.enums;
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    ROCK, PAPER, SCISSOR;
 
     public RockPaperScissorHandSign getWinner() {
+        switch(this) {
+            case ROCK:
+                return PAPER;
+            case PAPER:
+                return SCISSOR;
+            case SCISSOR:
+                return ROCK;
+        }
         return null;
     }
 
     public RockPaperScissorHandSign getLoser() {
+        switch(this) {
+            case ROCK:
+                return SCISSOR;
+            case PAPER:
+                return ROCK;
+            case SCISSOR:
+                return PAPER;
+        }
         return null;
     }
+
+//    public String getWinningMove(String handSign) {
+//        String[] handSigns = {"rock", "paper", "scissor"};
+//        String winningMove = "";
+//        for (int i = 0; i < handSigns.length; i++) {
+//            if (handSign.equals(handSigns[i])) {
+//                if(i+1 >= handSigns.length) {
+//                    i = -1;
+//                }
+//                winningMove = handSigns[i+1];
+//                break;
+//            }
+//        }
+//        return winningMove;
+//    }
 }
+
+
