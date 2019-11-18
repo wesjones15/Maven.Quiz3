9959673931b779001f0c55b87d53608673cfb1bb
wes <wes.jones1597@gmail.com>
2019-11-18 13:51:40 -0500

    SquareArrayAnalyzer tests passing

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
@@ -14,6 +16,18 @@ public class SquareArrayAnalyzer {
      * @return true if each `inputArray[i]` has a corresponding `squaredValues[j]` such that `squaredValues[j] == inputArray[i] * inputArray[i]`
      */
     public static Boolean compare(Integer[] inputArray, Integer[] squaredValues) {
-        return null;
+        // sort & square inputArray
+        //
+//        for (Integer value: inputArray) {
+//            Integer square = value * value;
+//            if ()
+//        }
+        return Arrays
+                .stream(inputArray)
+                .allMatch(value -> contains(squaredValues, (value*value)));
+    }
+
+    public static Boolean contains(Integer[] array, Integer value) {
+        return Arrays.stream(array).anyMatch(num -> num.equals(value));
     }
 }
