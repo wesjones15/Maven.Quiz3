16b79acf73287b89530d37ddc8a42326d9bb7da8
wes <wes.jones1597@gmail.com>
2019-11-19 08:19:08 -0500

    all tests passing. GenericArrayUtility filter method converted generic type <SomeType> to <T>

--- src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
@@ -2,45 +2,46 @@ package rocks.zipcode.io.quiz3.generics;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
  * @author leon on 09/12/2018.
  */
-public class ArrayUtility<SomeType> {
-    private final SomeType[] array;
+public class ArrayUtility<T> {
+    private final T[] array;
 
-    public ArrayUtility(SomeType[] array) {
+    public ArrayUtility(T[] array) {
         this.array = array;
     }
 
-    public SomeType findOddOccurringValue() {
+    public T findOddOccurringValue() {
         Integer count = 0;
-        for (SomeType value : array) {
+        for (T value : array) {
             count = getNumberOfOccurrences(value);
             if (count % 2 != 0) return value;
         }
         return null;
     }
 
-    public SomeType findEvenOccurringValue() {
+    public T findEvenOccurringValue() {
         Integer count = 0;
-        for (SomeType value : array) {
+        for (T value : array) {
             count = getNumberOfOccurrences(value);
             if (count % 2 == 0) return value;
         }
         return null;
     }
 
-    public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
         return (int) Arrays.stream(array)
                 .filter(val -> val == valueToEvaluate)
                 .count();
     }
 
-    //TODO come vack to this one
-    public SomeType[] filter(Function<SomeType, Boolean> predicate) {
-        return null;
+    public T[] filter(Function<T, Boolean> predicate) {
+        ArrayList<T> list = (ArrayList<T>) Arrays.stream(array).filter(val -> predicate.apply(val)).collect(Collectors.toList());
+        return list.toArray(Arrays.copyOf(array, list.size()));
     }
 }
